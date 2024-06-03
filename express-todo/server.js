b33ba56abd82853d5aa55df1fb64c22363f78169
Basanta-Kc <devbasanta@gmail.com>
2024-06-03 16:10:39 +0545

    crud for todo

--- express-todo/server.js
+++ express-todo/server.js
@@ -1,50 +1,59 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-const todos = [];
+// Database Connection
+mongoose.connect("mongodb://localhost:27017/mern-todo").then(() => {
+  console.log("MongodDb Connected.");
+});
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+});
+
+const Todo = mongoose.model("Todos", todoSchema);
 
 app.get("/", (req, res) => {
-  if (req.query.search) {
-    const filteredTodos = todos.filter((todo) =>
-      todo.includes(req.query.search)
-    );
-    res.render("index", {
-      todos: filteredTodos,
-      search: req.query.search,
-    });
-  }
-  res.render("index", { todos, search: null });
-});
-
-app.post("/todo", (req, res) => {
-  todos.push(req.body.todo);
-  res.redirect("/");
-});
-
-app.post("/todo/delete/:index", (req, res) => {
-  todos.splice(req.params.index, 1);
-  res.redirect("/");
-});
-
-app.get("/todo/edit/:index", (req, res) => {
-  const index = req.params.index;
-  res.render("edit", {
-    index,
-    todo: todos[index],
+  Todo.find().then((todos) => {
+    res.render("index", { todos, search: null });
   });
 });
 
-app.post("/todo/edit/:index", (req, res) => {
-  todos[req.params.index] = req.body.todo;
-  res.redirect("/");
+app.post("/todo", (req, res) => {
+  Todo.create({ title: req.body.todo })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 });
 
-app.get("/:username", (req, res) => {
-  res.send(req.params.username + " " + req.query.value);
+app.post("/todo/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.deleteOne({ _id: id }).then(() => {
+    res.redirect("/");
+  });
 });
+
+app.get("/todo/edit/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findOne({ _id: id }).then((todo) => {
+    res.render("edit", {
+      id,
+      todo,
+    });
+  });
+});
+
+app.post("/todo/edit/:id", (req, res) => {
+  Todo.updateOne({_id: req.params.id}, { title: req.body.todo }).then(() => {
+    res.redirect("/");
+  })
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server Started on Port 3000");

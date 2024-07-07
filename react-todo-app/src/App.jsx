6612c615e3e842b882bbc63f9a1c3daaf68e2529
Basanta-Kc <devbasanta@gmail.com>
2024-07-07 16:14:52 +0545

    edit & delete

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,6 +1,5 @@
-
-import './App.css'
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
 
 // const arr = [1,2]
 // const [firstnum, secondnum] = arr;
@@ -15,54 +14,96 @@ import {useState} from 'react'
 //   return [todos, setTodos]
 // }
 
-const arr = []
-const arr2 = arr
+// const arr = []
+// const arr2 = arr
 
-const num = [1,2,3]
-const num2 = [1,2,3]
+// const num = [1,2,3]
+// const num2 = [1,2,3]
 
-const nums = [1,2,3,4]
-const anotherNums = [...nums, 5,6,5]
+// const nums = [1,2,3,4]
+// const num2 = [...nums] // 1,2,34
+// const anotherNums = [...nums, 5,6,5] //
+// const isEditing = false;
 
-[1,2,3,4,5,6,5]
-
-
+// isEditing = true;
+// [1,2,3,4,5,6,5]
 
 function App() {
-
   // const todos = [leanrhtm, learn css, learn php]
-  const [todos, setTodos] = useState(['learn html', 'learn css', 'learn php']) // [['learn html', 'learn css', 'learn php'], () => {}]
+  const [todos, setTodos] = useState(["learn html", "learn css", "learn php"]); // [['learn html', 'learn css', 'learn php'], () => {}]
+  const [todo, setTodo] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [indexToBeEditied, setIndexToBeEdited] = useState(null);
 
-  const handleAddTodo = (e) => {
-    e.preventDefault()
-    const newTodo = e.target.elements[0].value
-    setTodos([...todos, newTodo])
-    e.target.reset()
-  }
+  const handleDelete = (index) => {
+    todos.splice(index, 1);
+    setTodos([...todos]);
+  };
 
-  const handleDelete = () => {
-    alert('delete clicked')
-  }
+  return (
+    <>
+      <h1>Todo app</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (isEditing) {
+            todos[indexToBeEditied] = todo;
+            setTodos([...todos]);
+          } else {
+            setTodos([...todos, todo]);
+          }
 
-  return <>
-
-     <h1>Todo app</h1> 
-     <form onSubmit={handleAddTodo}>
-        <input type="text" name="todo" placeholder='new todo' />
-        <input type="submit" value="add" />
-     </form>
-     <ul>
-      {todos.map((todo, index) => {
-        return <li key={index}>{todo} <button onClick={handleDelete}>delete</button></li>
-      })}
-     </ul>
-  </>
+          setTodo("");
+          setIsEditing(false);
+          setIndexToBeEdited(null);
+        }}
+      >
+        <input
+          type="text"
+          name="todo"
+          placeholder="new todo"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <input type="submit" value={isEditing ? "Update" : "Add"} />
+      </form>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              {todo}{" "}
+              <button
+                onClick={() => {
+                  handleDelete(index);
+                }}
+              >
+                delete
+              </button>
+              <button
+                onClick={() => {
+                  setIndexToBeEdited(index);
+                  setIsEditing(true);
+                  setTodo(todos[index]);
+                }}
+              >
+                Edit
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default App
-
+export default App;
 
 // todo App
 
 // learn html
 // learn ccaa
+
+const todos = ["learn html", "learn css", "learn php"];
+todos.splice(1, 1);
